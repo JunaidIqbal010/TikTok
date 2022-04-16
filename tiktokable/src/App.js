41a3b5d2d73f3155f63811e00530a09140ee2511
Junaid <junaidiqbalshah011@gmail.com>
2022-04-16 13:52:38 +0500

    initializing frontend for tiktok video

--- tiktokable/src/App.js
+++ tiktokable/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Video from './components/video/Video'
 function App() {
   return (
     <div className="App">
+      <h1> Developed by: Junaid Iqbal Shah</h1>
+      <div className="app_videos">
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
---
+++ tiktokable/src/components/video/Video.js
@@ -0,0 +1,11 @@
+import './Video.css';
+function Video() {
+  return (
+    <div className="video">
+      <h1>This is video</h1>
+      {/* <VideoFooter /> */}
+      {/* <VideoSidebar /> */}
+    </div>
+  );
+}
+export default Video;
