d1a57c1dfb3addc5ea21cf351959ea04a289537a
Junaid <junaidiqbalshah011@gmail.com>
2022-04-16 16:54:03 +0500

    giving initial touch to video module

--- tiktokable/src/components/video/Video.js
+++ tiktokable/src/components/video/Video.js
@@ -1,8 +1,27 @@
+import {useRef, useState} from "react";
 import './Video.css';
 function Video() {
+  const [play, setPlay] = useState(false);
+  const videoRef = useRef(null);
+  const onVideoPress = () => {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false)
+    }else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+    
+  };
   return (
     <div className="video">
-      <h1>This is video</h1>
+      <video
+        className="video__player"
+        loop 
+        onClick = {onVideoPress}
+        ref = {videoRef}
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        type="video/mp4"/>
     </div>
   );
 }
