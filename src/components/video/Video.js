5cb5401149239d3ad02e7467080176bc744eeed4
Junaid <junaidiqbalshah011@gmail.com>
2022-04-17 14:31:42 +0500

    integrating video module api with frontend

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -3,7 +3,15 @@ import './Video.css';
 import VideoFooter from "./VideoFooter";
 import VideoSidebar from "./VideoSidebar"
 
-function Video() {
+function Video({
+  video,
+  username,
+  description,
+  song,
+  likes,
+  comments,
+  shares
+}) {
   const [play, setPlay] = useState(false);
   const videoRef = useRef(null);
   const onVideoPress = () => {
@@ -23,17 +31,17 @@ function Video() {
         loop 
         onClick = {onVideoPress}
         ref = {videoRef}
-        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+        src= {video}
         type="video/mp4"/>
       <VideoFooter 
-        username= "jdshah_010"
-        description= "Check this out."
-        song= "I wanna be your lover boy."
+        username= {username}
+        description= {description}
+        song= {song}
       />
       <VideoSidebar
-        likes= {0}
-        comments= {111}
-        shares= {120}
+        likes= {likes}
+        comments= {comments}
+        shares= {shares}
       />
     </div>
   );
