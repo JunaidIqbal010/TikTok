a5d938a64fbf744f2bf5e8499a223b72d2635ced
Junaid <junaidiqbalshah011@gmail.com>
2022-04-17 11:20:36 +0500

    Ordering videos in descending order

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    @videos = Video.all.page(params[:page_no]).per(params[:per_page])
+    @videos = Video.order(created_at: :desc).page(params[:page_no]).per(params[:per_page])
     render json: @videos.map { |video|
       video.as_json.merge({ image_url: url_for(video.clip) })
     }
